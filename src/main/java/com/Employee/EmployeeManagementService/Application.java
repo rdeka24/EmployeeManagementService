08cff74cab2cb18c00c38044985d2d9d018cadee
rdeka24 <rupjyotideka2024@gmail.com>
2025-04-12 21:42:18 +0530

    Initial Commit
    Initial Commit

---
+++ src/main/java/com/Employee/EmployeeManagementService/Application.java
@@ -0,0 +1,14 @@
+package com.Employee.EmployeeManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("Welcome to Employee Management Service");
+	}
+
+}
