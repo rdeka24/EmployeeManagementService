b6a229c1199e25014553a3597de2e8d09ccfdfa9
IshaniKakoti <ishanikakoti97@gmail.com>
2025-06-22 23:08:45 +0530

    Exception done in salary

---
+++ src/main/java/com/Employee/EmployeeManagementService/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.Employee.EmployeeManagementService.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
+        // Get the first error message only
+        String errorMessage = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .findFirst()
+                .map(FieldError::getDefaultMessage)
+                .orElse("Invalid input");
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+}
+
